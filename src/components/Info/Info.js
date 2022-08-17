9856549041adb9c98b7fd2a9a2bcd0a477facdad
lmndovi <l.ndovi92@gmail.com>
2022-08-17 13:04:05 +0100

    modified:   src/components/Info/Info.js

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,7 +1,73 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Info = () => {
-  return <div>Info</div>;
+  return (
+    <div className="Info">
+      <div className="Info-navbar">
+        <h1>lu.ndovi</h1>
+      </div>
+      <div className="Info-firstPage">
+        <p>I turn dreams into digital products</p>
+      </div>
+      <div className="Info-coverPhoto">
+        <img
+          src="https://drive.google.com/uc?export=download&id=1sMAymaD2K1RTw3tKJlSvJM_fQlXuD04S"
+          alt="Lu Ndovi Cover Photo"
+        />
+      </div>
+      <div>
+        <p>About</p>
+        <p>
+          Hey, my name is Lu. I’m a multi-talented individual with an
+          unrelenting thirst for learning.
+        </p>
+        <p>
+          Having always admired renaissance men and their varied skill sets, I
+          place a lot of value in being competent across a wide range of
+          disciplines.
+        </p>
+        <p>
+          Depending on the day you can call me a developer, a copywriter, a
+          product designer or a consultant. My personality (INTJ) makes it
+          easier for me to break down problems into smaller logical steps in
+          order to find solutions. Being naturally positive, calm and empathetic
+          helps immensely when it comes to collaboration and in my mind makes me
+          the ultimate team player (see sporting background).
+        </p>
+      </div>
+      <div>
+        <p>
+          Aside from work I’ve been described as a semi-professional athlete,
+          social salsa dancer, everyday meditator, occasional yogi and lifetime
+          learner/explorer. All of these things relate back to my working life
+          as I am a firm believer in continual growth and constantly try to push
+          myself out of my comfort (which can be extremely difficult as a
+          natural introvert).
+        </p>
+        <p>
+          A burning desire of mine is to create a long lasting, sustainable
+          positive impact on the world but as well as my local community (north
+          London) and back in Zambia where my family hail from. Particularly I’d
+          love to focus on the youth and the vulnerable, who are all too often
+          forgotten about.
+        </p>
+      </div>
+      <div>
+        <p>
+          Currently I am working remotely with select clients but I am always
+          open to new opportunities that may present themselves. Below I have
+          attached my CV with more information on my education, experience,
+          skills and more:
+        </p>
+        <p>CV LINK:</p>
+        <p>
+          Click Here
+          (https://docs.google.com/document/d/1Vux-BkEdgev_z_5fb3OzWShJ8d_VvOLAzR9l-spU2fs/edit?usp=sharing)
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Info;
