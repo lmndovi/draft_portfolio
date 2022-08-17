d73bf1cd99c4655ca7fbcd56b1fe5b5da8d39968
lmndovi <l.ndovi92@gmail.com>
2022-08-17 12:43:27 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/Landing/Landing.css 	modified:   src/components/Landing/Landing.js

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Landing.css";
 
 const Landing = () => {
+  const [isHoverHello, setIsHoverHello] = useState(false);
+  const [isHoverIm, setIsHoverIm] = useState(false);
+  const [isHoverLu, setIsHoverLu] = useState(false);
+
   return (
     <div className="Landing grid-container">
       <div className="Landing-links">
-        <NavLink className="Landing-link1" to="/info">
-          Hello.
+        <NavLink
+          className="Landing-link1"
+          to="/info"
+          onMouseOver={() => setIsHoverHello(true)}
+          onMouseLeave={() => setIsHoverHello(false)}
+        >
+          {isHoverHello ? "Info" : "Hello."}
         </NavLink>
-        <NavLink className="Landing-link2" to="/work">
-          I'm
+
+        <NavLink
+          className="Landing-link2"
+          to="/work"
+          onMouseOver={() => setIsHoverIm(true)}
+          onMouseLeave={() => setIsHoverIm(false)}
+        >
+          {isHoverIm ? "Work" : "I'm"}
         </NavLink>
-        <NavLink className="Landing-link3" to="/contact">
-          Lu.
+        <NavLink
+          className="Landing-link3"
+          to="/contact"
+          onMouseOver={() => setIsHoverLu(true)}
+          onMouseLeave={() => setIsHoverLu(false)}
+        >
+          {isHoverLu ? "Contact" : "Lu."}
         </NavLink>
       </div>
       <div className="Landing-image">
